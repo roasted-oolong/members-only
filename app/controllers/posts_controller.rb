4f3300865bb8868984604272dfe3351e115dc23e
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-11-25 20:56:59 +0000

    Add create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.expect(post: [:title, :body])
+  end
 end
