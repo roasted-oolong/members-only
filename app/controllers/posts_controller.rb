21037c74c0ffba7ebf2412a7f5121537247809b0
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-11-25 19:38:20 +0000

    Add new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,7 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+ # before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
 end
